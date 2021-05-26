3349cfac90c842707c46d1cbcdef6950c84b5930
Luisa Ibagon <jeduard@unicauca.edu.co>
2021-05-26 18:50:06 -0500

    Commit que especifica la funcion para el manejo de las colisiones entre las barras y la pelota en el juego de Ping Pong

--- main.js
+++ main.js
@@ -26,10 +26,14 @@
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.speed_x = 5;
+        this.speed_x = 3;
         this.speed_y = 0;
         this.board = board;
         this.diretion = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
+
         board.ball = this;
         this.kind = "circle";
     }
@@ -38,6 +42,26 @@
         move: function () {
             this.x += (this.speed_x * this.diretion);
             this.y += (this.speed_y);
+        },
+        get width(){
+            return this.radius*2;
+        },
+        get height(){
+            return this.radius*2;
+        },
+        collision: function (bar) {
+            //Funcion para tratar las colisiones con las barras (raquetas)
+            var relative_interset_y = (bar.y + (bar.height)/2)-this.y;
+            var normalized_intersect_y = relative_interset_y / (bar.height/2);
+            this.bounce_angle = normalized_intersect_y*this.max_bounce_angle;
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) {
+                this.diretion = -1;
+            }else{
+                this.diretion = 1;
+            }
         }
     }
 
@@ -90,13 +114,47 @@
                 draw(this.ctx,el);
             };
         },
+        check_collision: function () {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)){ //Valida la colision entre elementos
+                    this.board.ball.collision(bar);
+                }                
+            }
+        },
         play: function(){
             if (this.board.playing) {
                 this.clean();
                 this.draw();
+                this.check_collision();
                 this.board.ball.move();
             }   
         }
+    }
+
+    //FUncion para la colision
+    function hit(a,b) {
+        var hit = false;
+        //colisiones horizontales
+        if(b.x + b.width >= a.x && b.x < a.x + a.width){
+            //Colision vertical
+            if (b.y + b.height >= a.y && b.y < a.y + a.height) {
+                hit = true;
+            }
+        }
+        //colision de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height) {
+                hit = true;
+            }
+        }
+        //colision de b con a
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height) {
+                hit = true;
+            }
+        }
+        return hit;
     }
 
     //Dibujar la raqueta
